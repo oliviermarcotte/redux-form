8d250e5ac3dc64d6012e080696ff20f3c83bf54e
Olivier Marcotte <oliviermarcotte@scolab.com>
2018-06-22 11:56:35 -0400

    build solution

---
+++ es/isReactNative.js
@@ -0,0 +1,7 @@
+var isReactNative =
+  typeof window !== 'undefined' &&
+  window.navigator &&
+  window.navigator.product &&
+  window.navigator.product === 'ReactNative'
+
+export default isReactNative
